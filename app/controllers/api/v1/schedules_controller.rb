71990164a6aca45d6609955f67e23fc0d0649405
Tilvin <kate2homy@gmail.com>
2016-05-09 14:10:22 +0300

    group tng

--- app/controllers/api/v1/schedules_controller.rb
+++ app/controllers/api/v1/schedules_controller.rb
@@ -2,20 +2,21 @@ class Api::V1::SchedulesController < ApplicationController
   before_action :set_tng, only: [:show, :edit, :update, :destroy]
 
   def index
-    tngs = Tng.all
-    render json: tngs.map { |tng| 
-        { id: tng.id,
-          town_id: tng.town.id,
-          town_name: tng.town.name,
-          trainer_id: tng.trainer.id,
-          trainer_name: tng.trainer.name,
-          discipline_id: tng.discipline_id,
-          discipline_name: tng.discipline.name,
-          day: tng.day, 
-          hall: tng.hall, 
-          time: tng.time
+    tngs = Tng.all.group_by {|tng| tng.day}
+      render json: tngs.map { |day, tng|
+        tng.map { |t|
+          { day: t.day,
+            time: t.time,
+            town_id: t.town.id,
+            town_name: t.town.name,
+            trainer_id: t.trainer.id,
+            trainer_name: t.trainer.name,
+            discipline_id: t.discipline_id,
+            discipline_name: t.discipline.name, 
+            hall: t.hall 
         }  
       }
+    }  
   end  
   
   # def show
